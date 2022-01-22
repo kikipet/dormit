2bb387a467d6b78e6d45ee8b59728a8c4b29be09
kikipet <songkimwl@gmail.com>
2022-01-22 18:03:03 -0500

    different email contact link

--- client/src/components/modules/Footer.js
+++ client/src/components/modules/Footer.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './Footer.css'
+import "./Footer.css";
 
 function Footer(props) {
     return (
-        <menu className='footer-container'>
-            <Link to='/about' className='footer-link'>About</Link>
-            <Link to='mailto:dormspam-admin@mit.edu' className='footer-link'>Contact</Link>
+        <menu className="footer-container">
+            <Link to="/about" className="footer-link">
+                About
+            </Link>
+            <Link
+                to="#"
+                onClick={(e) => {
+                    window.location = "mailto:dormspam-admin@mit.edu";
+                    e.preventDefault();
+                }}
+                className="footer-link"
+            >
+                Contact
+            </Link>
         </menu>
-    )
+    );
 }
 
 export default Footer;
