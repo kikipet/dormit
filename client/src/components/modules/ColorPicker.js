22f4ffa787bf0b90f8f4de55a6c6c1b00c0facba
kikipet <songkimwl@gmail.com>
2022-01-22 23:51:48 -0500

    color picker stays open after clicking

--- client/src/components/modules/ColorPicker.js
+++ client/src/components/modules/ColorPicker.js
@@ -1,36 +1,42 @@
-import React, { useState } from 'react';
-import { SketchPicker } from 'react-color';
-import { IoEyedrop } from 'react-icons/io5';
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
+import { IoEyedrop } from "react-icons/io5";
 
-import './ColorPicker.css'
+import "./ColorPicker.css";
 
 function ColorPicker(props) {
     const [pick, setPick] = useState(false);
 
     return (
-        <div className='color-picker'>
-            <div className='color-picker-controls'>
-                <div 
-                    className='color-square' 
-                    style={{backgroundColor: props.color}}/>
-                <button 
-                    className='color-eyedrop' 
-                    name='picker'
-                    type='button'
-                    onClick={() => setPick(!pick)}>
-                        <IoEyedrop />
+        <div className="color-picker">
+            <div className="color-picker-controls">
+                <div className="color-square" style={{ backgroundColor: props.color }} />
+                <button
+                    className="color-eyedrop"
+                    name="picker"
+                    type="button"
+                    onClick={() => setPick(!pick)}
+                >
+                    <IoEyedrop />
                 </button>
             </div>
-            {pick ? 
-                <SketchPicker 
-                    className='color-sketchpicker'
-                    disableAlpha={true}
-                    presetColors={props.colors}
-                    color={props.color}
-                    width={180}
-                    onChange={props.onChange}/>
-                : null
-            }
+            <div
+                onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                }}
+            >
+                {pick ? (
+                    <SketchPicker
+                        className="color-sketchpicker"
+                        disableAlpha={true}
+                        presetColors={props.colors}
+                        color={props.color}
+                        width={180}
+                        onChange={props.onChange}
+                    />
+                ) : null}
+            </div>
         </div>
     );
 }
